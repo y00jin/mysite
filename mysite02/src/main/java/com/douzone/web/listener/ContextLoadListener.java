b4ad6d95c321ca784037433c532ab4d654ee450b
B <wjs2510@icloud.com>
2020-02-24 11:39:02 +0900

    update

--- mysite02/src/main/java/com/douzone/web/listener/ContextLoadListener.java
+++ mysite02/src/main/java/com/douzone/web/listener/ContextLoadListener.java
@@ -5,12 +5,10 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener
 public class ContextLoadListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent servletContextEvent)  { 
 		ServletContext context = servletContextEvent.getServletContext();
-//		context.setAttribute(name, object);
 		String contextConfigLocation = context.getInitParameter("contextConfigLocation");
 		System.out.println("Application Starts..." + contextConfigLocation);
 	}
