6a29cea33841bfc4e16814e2ebd89e1db0b87ef2
B <wjs2510@icloud.com>
2020-03-20 09:50:36 +0900

    main image test

--- mysite03/src/main/java/com/douzone/mysite/service/FileUploadService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/FileUploadService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUploadService {
 	
 	private static final String SAVE_PATH = "/mysite-uploads";
-	private static final String URL = "/assets/image";
+	private static final String URL = "/assets/mysite-image";
 	
 	public String restore(MultipartFile multipartFile) {
 		String url = "";
