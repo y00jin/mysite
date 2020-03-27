2bb76899a5e63d4e9aebc8040e6d53ab45a3b2b3
B <wjs2510@icloud.com>
2020-03-27 11:47:50 +0900

    mysite06 finish

--- mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,20 +1,75 @@
 package com.douzone.mysite.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.douzone.mysite.config.web.FileUploadConfig;
-import com.douzone.mysite.config.web.SecurityConfig;
+import com.douzone.security.AuthInterceptor;
+import com.douzone.security.AuthUserHandlerMethodArgumentResolver;
+import com.douzone.security.LoginInterceptor;
+import com.douzone.security.LogoutInterceptor;
 
-//@Configuration
-//@EnableAspectJAutoProxy
-//@ComponentScan({"com.douzone.mysite.controller","com.douzone.mysite.exception"})
-//@Import({SecurityConfig.class, MessageConfig.class, FileUploadConfig.class})
-public class WebConfig {
+@Configuration
+@PropertySource("classpath:com/douzone/mysite/config/config.properties")
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private Environment env;
+
+	// Argument Resolver
+	@Bean
+	public HandlerMethodArgumentResolver authUserHandlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(authUserHandlerMethodArgumentResolver());
+	}
+
+	// Interceptors
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+
+	@Bean
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// login
+		registry.addInterceptor(loginInterceptor()).addPathPatterns(env.getProperty("security.auth-url"));
+
+		// logout
+		registry.addInterceptor(logoutInterceptor()).addPathPatterns(env.getProperty("security.logout-url"));
+
+		// auth
+		registry.addInterceptor(authInterceptor()).addPathPatterns("/**").excludePathPatterns(env.getProperty("security.auth-url"))
+				.excludePathPatterns(env.getProperty("security.logout-url")).excludePathPatterns("/assets/**");
+	}
+
+	// Mvc Resources(URL Magic Mapping)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
+				.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation"));
+	}
 
 }
-
-//@Import({MvcConfig.class, SecurityConfig.class}) // 15:08 before
-//@Import({MvcConfig.class, SecurityConfig.class, MessageConfig.class}) // 15:16 before
