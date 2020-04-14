b4f196e5881b3f7a8c43cbb46ab659f59d2e8ec4
B <wjs2510@icloud.com>
2020-04-14 12:08:20 +0900

    feat: form validation & jquery exception

--- mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
@@ -1,14 +1,13 @@
 package com.douzone.mysite.controller.api;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.douzone.mysite.dto.JsonResult;
 import com.douzone.mysite.service.UserService;
 
 @Controller("apiUserController")
@@ -19,14 +18,14 @@ public class UserController {
 	private UserService userService;
 	
 	@ResponseBody
-	@RequestMapping("/checkemail")
-	public Map<String, Object> checkEmail(
+	@RequestMapping(value = "/checkemail", method = RequestMethod.GET)
+//	@GetMapping(value = "/checkemail")
+	public JsonResult checkEmail(
 			@RequestParam(value="email", required=true, defaultValue="") String email) {
 		
 		boolean exist = userService.existUser(email);
-		Map<String, Object> map = new HashMap<>();
-		map.put("result", exist? "exist" : "not exist");
 		
-		return map;
+		return JsonResult.success(exist);
+		
 	}
 }
